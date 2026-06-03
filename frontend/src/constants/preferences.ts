e52e7b284a48172786c8f2b1ea45827d9e4326fb
jinyang628 <chenjinyang4192@gmail.com>
2026-06-03 09:49:20 +0800

    Nuke react native code

--- frontend/src/constants/preferences.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const DISTANCE_MIN_METERS = 100;
-export const DISTANCE_MAX_METERS = 50000;
-export const BUDGET_OPTIONS = z.enum(['$', '$$', '$$$', '$$$$']);
-export const DIET_OPTIONS = z.enum(['Spicy ok', 'Vegetarian', 'Vegan', 'Pescatarian', 'No carbs']);
